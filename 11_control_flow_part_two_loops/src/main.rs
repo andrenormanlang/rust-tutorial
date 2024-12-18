bba33317a83248f6df5d0b00e6cf2944a97ed27d
andrenormanlang <andrenormanlang@gmail.com>
2024-12-18 17:36:37 +0100

    constants, shadowing, comments and control flow

---
+++ 11_control_flow_part_two_loops/src/main.rs
@@ -0,0 +1,18 @@
+// Repetition with Loops
+// Doing things over and over
+
+// Loop is the simplest way to repeat an action in Rust. It runs the code inside the loop block until you explicitly tell it to stop.
+// 1- loop
+// 2- while
+// 3- for
+
+// 1- loop
+// The loop keyword tells Rust to execute a block of code over and over again forever or until you explicitly tell it to stop.
+
+
+fn main() {
+    // Loop keyword
+    loop {
+        println!("Hello, world!");
+    }
+}
